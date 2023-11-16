294762ad4a72a4cf970823c61837f50b677c520b
Woound <zohaibali1546@gmail.com>
2023-11-16 16:28:31 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import NavBar from './components/NavBar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Registration from './pages/Registration';
 function App() {
-  return <div className='App'>Hello World!</div>;
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Registration />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
