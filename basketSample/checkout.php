c28621936e62c5a58f7d4ef878784d78ba9fb1b0
Ken131313 <146330779+Ken131313@users.noreply.github.com>
2023-12-06 14:24:35 +0000

    payment page(basic html+php)

--- basketSample/checkout.php
+++ basketSample/checkout.php
@@ -14,29 +14,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    // Retrieve payment details from the form
-    $stripeToken = $_POST['stripeToken'];
-
     // Retrieve user details from the form
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
     $address = $_POST['address'];
 
     // Insert payment information into the database
-    $paymentInsertQuery = "INSERT INTO payment (stripe_token) VALUES ('$stripeToken')";
-    if (mysqli_query($conn, $paymentInsertQuery)) {
-        echo "Payment details inserted successfully. ";
-    } else {
-        echo "Error inserting payment details: " . mysqli_error($conn);
-    }
+$paymentInsertQuery = "INSERT INTO payment (created_at) VALUES (CURRENT_TIMESTAMP)";
+if (mysqli_query($conn, $paymentInsertQuery)) {
+    // Get the auto-generated paymentId
+    $paymentId = mysqli_insert_id($conn);
 
-    // Insert user information into the database
-    $userInsertQuery = "INSERT INTO user (first_name, last_name, address) VALUES ('$firstName', '$lastName', '$address')";
+    // Insert user information into the database with the correct paymentId
+    $userInsertQuery = "INSERT INTO user (first_name, last_name, address, created_at, payment_id) VALUES ('$firstName', '$lastName', '$address', CURRENT_TIMESTAMP, '$paymentId')";
     if (mysqli_query($conn, $userInsertQuery)) {
         echo "User details inserted successfully.";
     } else {
         echo "Error inserting user details: " . mysqli_error($conn);
     }
+} else {
+    echo "Error inserting payment details: " . mysqli_error($conn);
+}
+
 
     // Close database connection
     mysqli_close($conn);
---
+++ basketSample/test (1).sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2023 at 03:22 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `payment_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `payment`
+--
+
+INSERT INTO `payment` (`payment_id`, `created_at`) VALUES
+(1, '2023-12-06 14:21:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `payment_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `address`, `created_at`, `payment_id`) VALUES
+(1, 'lok him', 'wong', '28 Pugin Gardens', '2023-12-06 14:21:43', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`payment_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `payment_id` (`payment_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`payment_id`) REFERENCES `payment` (`payment_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
