400ed4a3656012d5a9a57372332763c1ea97becd
Zohaib Ali <zohaibali1546@gmail.com>
2024-03-03 06:26:12 +0000

    Basket Controller and other small changes

--- option-11/app/Http/Controllers/ManageBasketController.php
+++ option-11/app/Http/Controllers/ManageBasketController.php
@@ -25,85 +25,158 @@ use Illuminate\Support\Facades\Redirect;
 
 class ManageBasketController extends Controller
 {
-    //
-
-    public function search() 
-{
-    $basket = Basket::where('userid', auth()->user()->userid)->where('status', 'open')->get();
-
-    $totalPrice = Basket::where('userid', auth()->user()->userid)->where('status', 'open')->sum('totalprice');
-    
-    $bikes = [];
-    foreach ($basket as $item) {
-
-        $bike = Bikes::where('bikeid', $item->bikeid)->first();
-        $bikePart = BikePart::where('bikepartsid', $item->bikepartsid)->first();
-        $clothes = Clothes::where('clothingid', $item->clothingid)->first();
-        $repairkits = RepairKit::where('repairkitsid', $item->repairkitsid)->first();
-        $accessory = Accessory::where('accessoryid', $item->accessoryid)->first();
-        if ($bike) {
+    protected $bikes;
+    protected  $basket;
 
 
-            $bikes[] = $bike;
-            
-        } else if ($bikePart) {
-
-            $bikes[] = $bikePart;
+    public function search()
+    {
+        $this->basket = Basket::where('userid', auth()->user()->userid)->where('status', 'open')->get();
 
 
-        } else if ($clothes) {
+        $this->bikes = [];
+        foreach ($this->basket as $item) {
+
+            $bike = Bikes::where('bikeid', $item->bikeid)->first();
+            $bikePart = BikePart::where('bikepartsid', $item->bikepartsid)->first();
+            $clothes = Clothes::where('clothingid', $item->clothingid)->first();
+            $repairkits = RepairKit::where('repairkitsid', $item->repairkitsid)->first();
+            $accessory = Accessory::where('accessoryid', $item->accessoryid)->first();
+            if ($bike) {
 
 
-            $bikes[] = $clothes;
+                $this->bikes[] = $bike;
+            } else if ($bikePart) {
+
+                $this->bikes[] = $bikePart;
+            } else if ($clothes) {
 
 
-            
-        }  else if ($repairkits) {
+                $this->bikes[] = $clothes;
+            } else if ($repairkits) {
 
 
-            $bikes[] = $repairkits;
+                $this->bikes[] = $repairkits;
+            } else if ($accessory) {
 
 
-            
-        }  else if ($accessory) {
-
-
-            $bikes[] = $accessory;
-
-
-            
+                $this->bikes[] = $accessory;
+            }
         }
 
+        $totalPrice = Basket::where('userid', auth()->user()->userid)->where('status', 'open')->sum('totalprice');
 
 
+        return Inertia::render('Basket', ['basket' => $this->basket, 'totalprice' => $totalPrice, 'bikes' => $this->bikes]);
     }
-    
-    
-    return Inertia::render('Basket',['basket' => $basket, 'totalprice' => $totalPrice, 'bikes' => $bikes ]);
 
-    
+
+    public function update($basket)
+    {
+
+        $basket1 = Basket::where('userid', auth()->user()->userid)->where('status', 'open')->get();
+
+
+
+
+
+        foreach ($basket1 as $item) {
+
+            $bikes = Bikes::where('bikeid', $item->bikeid)->first();
+            $bikePart = BikePart::where('bikepartsid', $item->bikepartsid)->first();
+            $clothes = Clothes::where('clothingid', $item->clothingid)->first();
+            $repairkits = RepairKit::where('repairkitsid', $item->repairkitsid)->first();
+            $accessory = Accessory::where('accessoryid', $item->accessoryid)->first();
+
+            if ($bikes) {
+
+
+                $basket->totalprice = $basket->quantity * $bikes->price;
+            } else if ($bikePart) {
+
+                $basket->totalprice = $basket->quantity * $bikePart->price;
+            } else if ($clothes) {
+
+                $basket->totalprice = $basket->quantity * $clothes->price;
+            } else if ($repairkits) {
+
+
+                $basket->totalprice = $basket->quantity * $repairkits->price;
+            } else if ($accessory) {
+
+
+                $basket->totalprice = $basket->quantity * $repairkits->accessory;
+            }
+        }
+    }
+
+    public function addRemItem(Request $request)
+    {
+
+        //When adding or removing items need to change ht price
+
+        if ($request->action == "delete") {
+
+            $basket = Basket::where('basketid', $request->basketid)->first();
+
+
+            $basket->delete();
+
+            
+
+            return Redirect::route('basket');
+
+        } else if ($request->action == "add") {
+
+
+            $basket = Basket::where('basketid', $request->input('basketid'))->first();
+
+
+
+            $basket->quantity =  $basket->quantity + 1;
+
+            $this->update($basket);
+            $basket->save();
+
+            return Redirect::route('basket');
+        } else if ($request->action == "remove") {
+
+            $basket = Basket::find($request->basketid);
+
+            if ($basket->quantity <= 1) {
+
+
+
+
+
+
+
+
+                return    $this->deleteProduct($request);
+            } else {
+
+
+                $basket->quantity =  $basket->quantity - 1;
+
+                $this->update($basket);
+
+
+                $basket->save();
+                return Redirect::route('basket');
+            }
+        }
+    }
+
+    public function deleteProduct(Request $request)
+    {
+
+
+        $basket = Basket::where('userid', auth()->user()->userid)->get();
+        $basketid = $request->basketid;
+
+        $basketFind = Basket::where('basketid', $basketid);
+
+        $basketFind->delete();
+        return $this->search();
+    }
 }
-
-
-public function deleteProduct(Request $request)
-{
- 
-    
-    $basket = Basket::where('userid', auth()->user()->userid)->get();
-    $basketid =$request->input('basketid');
-
-    $basketFind = Basket::where('basketid', $basketid);
-
-    $basketFind->delete();
-
- 
-
-  
-}
-
-
-
-
-}
-
-
