d5b73b7ae9521fe0f70c2de4af74d2fd45e1d9ac
Maciej Majorczyk <70149135+MaciejDotDev@users.noreply.github.com>
2024-03-16 16:28:15 +0000

    Modified Database Seeder to automate Deployment

--- option-11/database/seeders/DatabaseSeeder.php
+++ option-11/database/seeders/DatabaseSeeder.php
@@ -3,20 +3,24 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UsersTableSeeder::class,
+            BikesTableSeeder::class,
+            AccessoryTableSeeder::class,
+            ClothingSeeder::class,
+            RepairKitsTableSeeder::class,
+            ProductPartsSeeder::class,
+            AdminsTableSeeder::class,
+            UsersTableSeeder::class,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
